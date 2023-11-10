6e0c274479b9e186405522c5ae6d2c7732633071
Thomas Pazhaidam <83510131+ThomasPazhaidam@users.noreply.github.com>
2023-11-09 23:23:32 -0500

    finished main

---
+++ main.c
@@ -0,0 +1,168 @@
+/*******************************************************************************
+* Filename              :   main.c
+* Author                :   Thomas Pazhaidam
+* Origin Date           :   11/17/2023
+* Version               :   0.0.1
+* Compiler              :   uVision5 
+* Notes                 :   None
+*******************************************************************************/
+/******************************************************************************
+* Includes
+*******************************************************************************/
+#define osObjectsPublic                     // define objects in main module
+#include "osObjects.h"                      // RTOS object definitions
+#include "cmsis_os.h"                       // CMSIS RTOS header file
+#include <stdio.h>
+#include "RTE_Components.h"            		 // Component selection
+#include "GLCD.h"      
+#include "KBD.h"
+/******************************************************************************
+* Module Preprocessor Constants
+*******************************************************************************/
+
+/******************************************************************************
+* Module Preprocessor Macros
+*******************************************************************************/
+
+/******************************************************************************
+* Module Typedefs
+*******************************************************************************/
+
+/******************************************************************************
+* Module Variable Definitions
+*******************************************************************************/
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+/******************************************************************************
+* Function Externs
+*******************************************************************************/
+extern void LaunchMusicPlayer (void);
+/******************************************************************************
+* Function Definitions
+*******************************************************************************/
+/******************************************************************************
+* Initialize device modules
+*******************************************************************************/
+int Initialize_LPC1768()
+{
+	KBD_Init();
+	GLCD_Init();
+	GLCD_Clear(White);
+	GLCD_SetBackColor(White);
+	GLCD_SetTextColor(Black);
+	GLCD_DisplayString(0, 4, 1, (unsigned char*)"Main Menu");
+	
+	return 0;
+}
+
+/******************************************************************************
+* Delay function
+*******************************************************************************/
+void Delay(int multiplier) 
+{
+	int c = 0;
+	while( c++ < (multiplier*100)){}
+}
+/******************************************************************************
+* update main menu item index
+*******************************************************************************/
+int UpdateItemSelection(int ItemIndex, int JoystickVal) 
+{
+	int newItemIndex = 0;
+	switch(JoystickVal){
+		case KBD_UP:
+			if(ItemIndex == 0)
+			{
+				newItemIndex = 2;
+			}
+			else
+			{
+				newItemIndex = ItemIndex - 1;
+			}
+			break;
+		case KBD_DOWN:
+			if(ItemIndex == 2)
+			{
+				newItemIndex = 0;
+			}
+			else
+			{
+				newItemIndex = ItemIndex + 1;
+			}			
+			break;
+		}	
+
+		return newItemIndex;
+}
+
+/******************************************************************************
+* update what is displayed in main menu based on item index
+*******************************************************************************/
+void UpdateGlcdItemSelection(int ItemIndex) 
+{
+	
+	unsigned char itemNames[3][256] = {"Gallery", "Music Player", "The Woods"};
+	int i = 0;
+	
+	GLCD_SetBackColor(White);
+	GLCD_SetTextColor(Blue);
+	
+	for(i = 0; i<3; i++)
+	{
+		if(i == ItemIndex)
+		{
+			GLCD_SetBackColor(Blue);
+			GLCD_SetTextColor(White);
+		}
+		else
+		{
+			GLCD_SetBackColor(White);
+			GLCD_SetTextColor(Black);
+		}
+		GLCD_DisplayString(i+4, 0, 1, itemNames[i]);
+	}
+}
+
+/******************************************************************************
+* Launch selected application
+*******************************************************************************/
+void OpenSelectedItem(int ItemIndex) 
+{
+	switch (ItemIndex)
+	{
+	case 0:
+		break;
+	case 1:
+		LaunchMusicPlayer();
+		break;
+	case 2:
+		break;
+	}
+	GLCD_Clear(White);
+	GLCD_SetBackColor(White);
+	GLCD_SetTextColor(Black);
+	GLCD_DisplayString(0, 4, 1, (unsigned char*)"Main Menu");
+}
+/******************************************************************************
+* main.c
+*******************************************************************************/
+int main( void )
+{
+	int joystickVal = 0;
+	int itemIndex = 0;
+	Initialize_LPC1768();
+	
+	while(1)
+	{
+		joystickVal = get_button();
+		itemIndex = UpdateItemSelection(itemIndex, joystickVal);
+		UpdateGlcdItemSelection(itemIndex);
+		if(joystickVal == KBD_SELECT)
+		{
+			OpenSelectedItem(itemIndex);
+		}
+		Delay(60000);
+	}
+}
